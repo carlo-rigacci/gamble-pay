233cb678cad5456674b77c2c1148c95a4f2e596e
Carlo Rigacci <72162133+carlo-rigacci@users.noreply.github.com>
2025-06-06 23:48:36 +0200

    init

---
+++ src/main/java/com/faintful/casino/gamble_pay/GamblePayApplication.java
@@ -0,0 +1,13 @@
+package com.faintful.casino.gamble_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamblePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamblePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faintful/casino/gamble_pay/GamblePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faintful.casino.gamble_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamblePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
